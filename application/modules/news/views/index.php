564579bf45867c612a15acef8a2b554cb1b9d8c7
FASHAH\Fashah Darullah <fashahd@gmail.com>
2018-05-15 09:59:28 +0700

    update

--- application/modules/news/views/index.php
+++ application/modules/news/views/index.php
@@ -21,60 +21,65 @@
                     <?=$optsidePage?>
                 </div>
             </div>
-            <div class="col-md-8">
+            <div class="col-md-9">
                 <div class="row ">
                     <div id="column_iCycp" class="hc_column_cnt col-sm-12">
-                        <div class="row">
-                            <div class="col-md-12 hc_title_tag_cnt">
-							<h1 id="rBIkI">News</h1></div>
-							<div class="col-md-12 hc_separator_cnt"><hr class="default"/></div>
-							<div class="row">
-								<div class="col-md-12 hc_pt_grid_list_cnt">
-									<div class="grid-list one-row-list" style="">
-										<div class="grid-item col-md-12" style='padding:30px'>
-											<div class="advs-box-content">
-												<div class="tag-row icon-row">
-													<span><i class="fa fa-calendar"></i><a> 15 Aug 2017</a></span>
-												</div>
-												<h3>
-													Disukai Konsumen, Tekiro Rebut Top Brand 4 Tahun Berturut-turut
-												</h3>
-												<p style="text-align:left">JAKARTA - Tekiro tools kembali mendapatkan Top Brand Award untuk kategori Handtools di tahun 2017. Penghargaan ini menjadi penghargaan keempat yang di terima Tekiro secara berturut-turut sejak 2014 lalu.
-Penghargaan itu merupakan bukti nyata atas kepercayaan konsumen yang telah menggunakan produk Tekiro selama bertahun-tahun. Tekiro merupakan perusahaan yang sudah 20 tahun melayani kebutuhan masyarakat terhadap peralatan pendukung keperluannya sehari-hari, termasuk di dalamnya kebutuhan di industri automotif.</p>
-												<h3><a>Read More ...</a></h3>
-											</div>
-										</div>
-									</div>
+                        <div class="maso-list maso-30 list-sm-6">
+							<div class="navbar navbar-inner">
+								<div class="navbar-toggle"><i class="fa fa-bars"></i><span>MENU</span><i class="fa fa-angle-down"></i></div>
+								<div class="collapse navbar-collapse">
+									<ul class="nav navbar-nav over inner ms-minimal maso-filters">
+										<li class="active"><a data-filter="maso-item">Latest News</a></li>
+										<li><a data-filter="cat1">Press Release</a></li>
+										<li><a class="maso-order" data-sort="asc"><i class="fa fa-arrow-down"></i></a></li>
+									</ul>
 								</div>
 							</div>
-							<div class="row">
-								<div class="col-md-12 hc_pt_grid_list_cnt">
-									<div class="grid-list one-row-list" style="">
-										<div class="grid-item col-md-12" style='padding:30px'>
-											<div class="advs-box-content">
-												<div class="tag-row icon-row">
-													<span><i class="fa fa-calendar"></i><a> 15 Aug 2017</a></span>
-												</div>
-												<h3>
-													Tips Merawat Kunci Sock
-												</h3>
-												<p style="text-align:left">
-													Pengendara.com - Perkakas tersebut rusak, karatan bahkan hilang, sudah pasti akan merepotkan dan membuat kesal kita sebagai pemiliknya. Pengendara.com memberikan referensi tips merawat kunci yang dipersembahkan Tekiro. Berikut tipsnya.
+							<div class="maso-box row">
+								<div data-sort="2" class="maso-item col-md-12 cat1 cat3">
+									<div class="advs-box advs-box-side" data-anima="fade-left" data-trigger="hover">
+										<div class="row">
+											<div class="col-md-4">
+												<div class="img-box"><img src="<?=base_url()?>appsources/images/gallery/image-1.jpg" alt=""></div>
+											</div>
+											<div class="col-md-8">
+												<h3>Virtual Reality Heads</h3>
+												<hr class="anima">
+												<p>
+													Magnam excepturi parturient ante magnis, ullamcorper commodo excepturi sollicitudin odio suspendisse, nostra. Cumque facilis assumenda
 												</p>
-												<h3><a>Read More ...</a></h3>
+												<a class="btn-text" href="#">Read more</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div data-sort="4" class="maso-item col-md-12 cat2 cat3">
+									<div class="advs-box advs-box-side" data-anima="fade-left" data-trigger="hover">
+										<div class="row">
+											<div class="col-md-4">
+												<div class="img-box"><img src="<?=base_url()?>appsources/images/gallery/image-2.jpg" alt=""></div>
+											</div>
+											<div class="col-md-8">
+												<h3>Yellow Business design</h3>
+												<hr class="anima">
+												<p>
+													Magnam excepturi parturient ante magnis, ullamcorper commodo excepturi sollicitudin odio suspendisse, nostra. Cumque facilis assumenda
+												</p>
+												<a class="btn-text" href="#">Read more</a>
 											</div>
 										</div>
 									</div>
 								</div>
 							</div>
-                        </div>
+						</div>
                     </div>
                 </div>
+				<div class="col-md-12 hc_separator_cnt"><hr class="default"/></div>
 				<div class="pagination-inner list-nav">
 					<ul class="pagination-lg hide-first-last pagination-grid pagination" data-page-items="6" data-options="scrollTop:true,prev:<i class='fa fa-angle-left'></i> Prev,next:Next<i class='fa fa-angle-right'></i>">
 					<li class="first disabled"><a href="#"><i class="fa fa-angle-double-left"></i> <span>First</span></a></li><li class="prev disabled"><a href="#"><i class="fa fa-angle-left"></i> Prev</a></li><li class="page active"><a href="#">1</a></li><li class="page"><a href="#">2</a></li><li class="next"><a href="#">Next<i class="fa fa-angle-right"></i></a></li><li class="last"><a href="#"><span>Last</span> <i class="fa fa-angle-double-right"></i></a></li></ul>
 				</div>
-            </div>
+			</div>
         </div>
         <?php } ?>
         <?php if($module_id == "Event"){ ?>
@@ -85,54 +90,36 @@ Penghargaan itu merupakan bukti nyata atas kepercayaan konsumen yang telah mengg
                 </div>
             </div>
             <div class="col-md-8">
-                <div class="row ">
-                    <div id="column_iCycp" class="hc_column_cnt col-sm-12">
-                        <div class="row">
-                            <div class="col-md-12 hc_title_tag_cnt">
-							<h1 id="rBIkI">Event</h1></div>
-							<div class="col-md-12 hc_separator_cnt"><hr class="default"/></div>
-							<div class="row">
-								<div class="col-md-12 hc_pt_grid_list_cnt">
-									<div class="grid-list one-row-list" style="">
-										<div class="grid-item col-md-12" style='padding:30px'>
-											<div class="advs-box-content">
-												<div class="tag-row icon-row">
-													<span><i class="fa fa-calendar"></i><a> 15 Aug 2017</a></span>
-												</div>
-												<h3>
-													Disukai Konsumen, Tekiro Rebut Top Brand 4 Tahun Berturut-turut
-												</h3>
-												<p style="text-align:left">JAKARTA - Tekiro tools kembali mendapatkan Top Brand Award untuk kategori Handtools di tahun 2017. Penghargaan ini menjadi penghargaan keempat yang di terima Tekiro secara berturut-turut sejak 2014 lalu.
-Penghargaan itu merupakan bukti nyata atas kepercayaan konsumen yang telah menggunakan produk Tekiro selama bertahun-tahun. Tekiro merupakan perusahaan yang sudah 20 tahun melayani kebutuhan masyarakat terhadap peralatan pendukung keperluannya sehari-hari, termasuk di dalamnya kebutuhan di industri automotif.</p>
-												<h3><a>Read More ...</a></h3>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-md-12 hc_pt_grid_list_cnt">
-									<div class="grid-list one-row-list" style="">
-										<div class="grid-item col-md-12" style='padding:30px'>
-											<div class="advs-box-content">
-												<div class="tag-row icon-row">
-													<span><i class="fa fa-calendar"></i><a> 15 Aug 2017</a></span>
-												</div>
-												<h3>
-													Tips Merawat Kunci Sock
-												</h3>
-												<p style="text-align:left">
-													Pengendara.com—Perkakas tersebut rusak, karatan bahkan hilang, sudah pasti akan merepotkan dan membuat kesal kita sebagai pemiliknya. Pengendara.com memberikan referensi tips merawat kunci yang dipersembahkan Tekiro. Berikut tipsnya.
-												</p>
-												<h3><a>Read More ...</a></h3>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-                        </div>
-                    </div>
-                </div>
+				<div class="maso-list gallery">
+					<div class="navbar navbar-inner">
+						<div class="navbar-toggle"><i class="fa fa-bars"></i><span>Menu</span><i class="fa fa-angle-down"></i></div>
+						<div class="collapse navbar-collapse">
+							<ul class="nav navbar-nav over ms-minimal inner maso-filters">
+								<li class="current-active active"><a data-filter="maso-item">Kegiatan Internal</a></li>
+								<li><a data-filter="cat1">Kegiatan Eksternal</a></li>
+								<li><a class="maso-order" data-sort="asc"><i class="fa fa-arrow-down"></i></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="maso-box row" data-lightbox-anima="fade-top">
+						<div data-sort="1" class="maso-item col-md-4 cat1 cat2 cat4">
+							<a class="img-box" href="<?=base_url()?>appsources/images/gallery/image-13.jpg" data-lightbox-anima="fade-top">
+								<img src="<?=base_url()?>appsources/images/gallery/image-13.jpg" alt="" />
+							</a>
+						</div>
+						<div data-sort="1" class="maso-item col-md-4 cat2 cat4">
+							<a class="img-box" href="<?=base_url()?>appsources/images/gallery/image-13.jpg" data-lightbox-anima="fade-top">
+								<img src="<?=base_url()?>appsources/images/gallery/image-14.jpg" alt="" />
+							</a>
+						</div>
+						<div data-sort="1" class="maso-item col-md-4 cat1 cat2">
+							<a class="img-box" href="<?=base_url()?>appsources/images/gallery/image-13.jpg" data-lightbox-anima="fade-top">
+								<img src="<?=base_url()?>appsources/images/gallery/image-16.jpg" alt="" />
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-12 hc_separator_cnt"><hr class="default"/></div>
 				<div class="pagination-inner list-nav">
 					<ul class="pagination-lg hide-first-last pagination-grid pagination" data-page-items="6" data-options="scrollTop:true,prev:<i class='fa fa-angle-left'></i> Prev,next:Next<i class='fa fa-angle-right'></i>">
 						<li class="first disabled"><a href="#"><i class="fa fa-angle-double-left"></i> <span>First</span></a></li>
